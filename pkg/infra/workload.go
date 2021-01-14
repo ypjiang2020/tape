a214229cefeb02026bcc3a0fd050ff5d17d2e8d2
yunpeng jiang <yunpeng@heming-rdma6.cs.hku.hk>
2021-01-14 20:43:02 +0800

    generate workload for smallbank

--- pkg/infra/workload.go
+++ pkg/infra/workload.go
@@ -1,13 +1,20 @@
 package infra
 
 import (
-	// "fmt"
+	"strconv"
+	"fmt"
+	"os"
 	"math/rand"
 	"time"
+	"bufio"
+	"strings"
 )
 
 
 var chs = []rune("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()_=")
+var accounts_file string = "ACCOUNTS"
+var transactions_file string = "TRANSACTIONS"
+var accounts []string
 
 
 func getName(n int) string {
@@ -19,32 +26,81 @@ func getName(n int) string {
 }
 
 func generate() []string {
-	// idx := getName(64)
-	// res := fmt.Sprintf("{\"Args\":[\"create_account\", \"%s\", \"%s\", \"1000000\", \"1000000\"]}", idx, idx)
-	var res []string
-	res = append(res, "CreateAccount")
-	res = append(res, "1217")
-	res = append(res, "Bob")
-	res = append(res, "100")
-	res = append(res, "90")
-	// res = append(res, "Query")
-	// res = append(res, "1217")
+	transactions_type := "transfer_money"
+	if _, err := os.Stat(accounts_file); os.IsNotExist(err) {
+		transactions_type = "create_accounts"
+	}
 
-	// res = append(res, "CreateCar")
-	// res = append(res, idx)	
-	// res = append(res, "china")
-	// res = append(res, "tesla")
-	// res = append(res, "bluegreen")
-	// res = append(res, "yunpeng")
+	var res []string
+	// fmt.Println(transactions_type)
+
+	if transactions_type == "transfer_money" {
+		src := rand.Intn(len(accounts))
+		dst := rand.Intn(len(accounts)) // maybe same
+		// TODO: support other transactions 
+		// (e.g., Amalgamate, TransactionsSavings, WriteCheck, DepositChecking)
+		res = append(res, "SendPayment") 
+		res = append(res, accounts[src])
+		res = append(res, accounts[dst])
+		res = append(res, "1")
+	} else {
+	   	idx := getName(64)
+		res = append(res, "CreateAccount")
+		res = append(res, idx)
+		res = append(res, idx)
+		res = append(res, strconv.Itoa(1e9))
+		res = append(res, strconv.Itoa(1e9))
+	}
 	return res
 }
 
 func GenerateWorkload(n int) [][]string {
 	rand.Seed(time.Now().UnixNano())
+
+	if _, err := os.Stat(accounts_file); os.IsNotExist(err) {
+		fmt.Printf("create %d accounts\n", n)
+	} else {
+		fmt.Printf("transfer money: %d\n", n)
+
+		f, _ := os.Open(accounts_file)
+		input := bufio.NewScanner(f) 
+		for input.Scan() {
+			accounts = append(accounts, input.Text())
+		}
+		fmt.Printf("read %d accounts from %s\n", len(accounts), accounts_file)
+	}
+
 	i := 0
 	var res [][]string
 	for i = 0; i < n; i++ {
 		res = append(res, generate())
 	}
+	if _, err := os.Stat(accounts_file); err == nil {
+		// save transactions to file for checking
+		os.Remove(transactions_file)
+		f, err := os.Create(transactions_file) 
+		if err != nil {
+			fmt.Println("create file failed", err)
+		}
+		defer f.Close()
+
+		for i = 0; i < n; i++ {
+			f.WriteString(strings.Join(res[i], " ") )
+			f.WriteString("\n")
+		}
+
+	} else {
+		// save accounts to file 
+		f, err := os.Create(accounts_file) 
+		if err != nil {
+			fmt.Println("create file failed", err)
+		}
+		defer f.Close()
+
+		for i = 0; i < n; i++ {
+			f.WriteString(res[i][1] + "\n")
+		}
+	}
+
 	return res
 }
