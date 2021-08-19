2978c3fe3aa63eac4247fe5b7dd34ec7558184d1
yunpeng <ypjiang@cs.hku.hk>
2021-08-19 21:17:28 +0800

    fix

--- pkg/infra/proposer.go
+++ pkg/infra/proposer.go
@@ -45,7 +45,9 @@ func (ps *Proposers) Start(signed []chan *Elements, processed chan *Elements, do
 		// peer connection should be config.ClientPerConn * config.NumOfConn
 		for k := 0; k < config.ClientPerConn; k++ {
 			for j := 0; j < config.NumOfConn; j++ {
-				go ps.workers[i][j].Start(signed[i], processed, done, int(len(config.Endorsers)/config.EndorserGroups), k, j)
+				tempk := k
+				tempj := j
+				go ps.workers[i][j].Start(signed[i], processed, done, int(len(config.Endorsers)/config.EndorserGroups), tempk, tempj)
 			}
 		}
 	}
